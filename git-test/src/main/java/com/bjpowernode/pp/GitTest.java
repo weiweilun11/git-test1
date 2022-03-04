a26e4c85e340a8f160072b6d5c629c8334f716d8
wwl <weiweilun11@126.com>
2022-03-04 18:36:01 +0800

    首次提交

---
+++ git-test/src/main/java/com/bjpowernode/pp/GitTest.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.pp;
+
+/**
+ * ClassName:GitTest
+ * Package:com.bjpowernode.pp
+ * Description: 描述信息
+ *
+ * @date:2022/3/4 18:29
+ * @author:动力节点
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("idea get");
+    }
+}
