5bf6d6a717fed1b81f7b8cfc902b64bc7cbb8b19
wwl <weiweilun11@126.com>
2022-03-04 19:20:49 +0800

    fix22

--- git-test/src/main/java/com/bjpowernode/pp/GitTest.java
+++ git-test/src/main/java/com/bjpowernode/pp/GitTest.java
@@ -15,6 +15,6 @@ public class GitTest {
         System.out.println("11");
         System.out.println("22");
         System.out.println("33");
-        System.out.println("44");
+        System.out.println("aa");
     }
 }
